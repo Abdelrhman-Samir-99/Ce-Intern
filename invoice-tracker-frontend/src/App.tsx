8843a651c632d588c1eedd9b8633efbb06928fe5
Abdelrhman Samir <abdulrhman.samir123@gmail.com>
2022-08-15 18:23:07 +0200

    Adding IT-front

---
+++ invoice-tracker-frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.scss";
+import Page1 from "./pages/fake-page1/page1";
+import Page2 from "./pages/fake-page2/page2";
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navigate to="/page1" replace />} />
+        <Route path="page1" element={<Navbar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
